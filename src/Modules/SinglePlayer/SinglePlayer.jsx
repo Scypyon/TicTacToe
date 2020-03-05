65e711bcb3fcc19e8085739968b0a7de8c022739
Wojciech Michałowski <wojtek1214.99@wp.pl>
2020-03-05 21:49:18 +0100

    small refactor

--- src/Modules/SinglePlayer/SinglePlayer.jsx
+++ src/Modules/SinglePlayer/SinglePlayer.jsx
@@ -3,21 +3,11 @@ import { Helmet } from "react-helmet";
 import TableToGame from "./components/TableToGame";
 import { newGame } from "./singlePlayerActions";
 import { useDispatch, useSelector } from "react-redux";
+import { winningConditions } from "constants/singlePlayerConst";
 
 export default function SinglePlayer({ match }) {
   const dataTable = useSelector(state => state.singePlayerReducer);
   const [winner, setWinner] = useState(false);
-
-  const winningConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
 
   useEffect(() => {
     if (!winner) {
